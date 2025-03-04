60e424b2cd5fc4b9e8ef2782b5d271dab4982f30
harireddy7 <harimsd8@gmail.com>
2025-03-04 22:28:39 +0530

    add endpoints to get and update secrets

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const { PubSub } = require('@google-cloud/pubsub');
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const crypto = require('crypto');
 
 const app = express();
-const pubsub = new PubSub();
 const secretManager = new SecretManagerServiceClient();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send('Healthy')
-})
+const generateKey = () => {
+  return crypto.randomBytes(8).toString('hex')
+}
+
+
+app.get('/', (_, res) => {
+  res.status(200).send('Healthy');
+});
+
 
 app.post('/', async (req, res) => {
-  console.log(`REQ START::: GSM secret path:: ${process.env.SECRET_PATH}`)
+  console.log(`REQ START::: GSM secret path:: ${process.env.SECRET_PATH}`);
+
   const message = req.body.message;
   console.log(JSON.stringify(req.body));
 
@@ -21,19 +27,49 @@ app.post('/', async (req, res) => {
   console.log(`Received message: ${data}`);
 
   // Update the secret in Secret Manager
-  const newSecret = 'new-secret';
+  const secretValue = generateKey();
   const [version] = await secretManager.addSecretVersion({
     parent: process.env.SECRET_PATH,
     payload: {
-      data: Buffer.from(newSecret, 'utf-8'),
+      data: Buffer.from(secretValue, 'utf-8'),
     },
   });
 
-  console.log(`Added secret version: ${version.name} - ${newSecret}`);
+  console.log(`Added secret version: ${version.name} - ${secretValue}`);
   res.status(204).send();
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+
+app.get('/secret', async (req, res) => {
+  const [version] = await secretManager.accessSecretVersion({
+    name: `${process.env.SECRET_PATH}/versions/latest`,
+  });
+
+  res.status(200).send(version.payload?.data?.toString());
+})
+
+
+app.post('/secret', async (req, res) => {
+  // Update the secret in Secret Manager
+  const secretValue = generateKey();
+  const [version] = await secretManager.addSecretVersion({
+    parent: process.env.SECRET_PATH,
+    payload: {
+      data: Buffer.from(secretValue, 'utf-8'),
+    },
+  });
+
+  console.log(`Added secret version: ${version.name} - ${secretValue}`);
+  res.status(204).send(secretValue);
 });
+
+
+app.get('/logenv', async (req, res) => {
+  console.log(process.env);
+  res.status(200).send(process.env)
+})
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
